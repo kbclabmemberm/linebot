df85d58415893a78b882d833ecf0ed35887f65b5
Kota Takahashi <161806557+kurokitabc@users.noreply.github.com>
2024-04-30 01:56:38 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from decouple import config
+
+ACCESS_TOKEN = config("LINE_CHANNEL_ACCESS_TOKEN")
+SECRET = config("LINE_CHANNEL_SECRET")
+MONGO_API_KEY = config("MONGO_API_KEY")
+GROUP_ID = config("GROUP_ID")
