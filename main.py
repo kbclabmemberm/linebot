89949f71917eba9b078a72b0ca5051dd14600480
Kota Takahashi <161806557+kurokitabc@users.noreply.github.com>
2024-04-30 21:15:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,3 @@ app.include_router(route_practice.router)
 @app.get("/")
 def root():
     return {"message": "Welcome to Fast API"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
